662d20bd74d29de5991a2372981008ee19ed1acc
drogomarks <rudy.marks@gmail.com>
2015-02-12 02:02:29 -0600

    Create vhosts.sh

---
+++ vhosts.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+#added the ability to run the script locally with arguments in the event that justcurl.com is not avaialbe.
+
+while getopts "d:r:p:" opt; do
+   case $opt in
+      d) DOMAIN=$OPTARG ;;
+      r) DOCROOT=$OPTARG ;;
+      p) PORT=$OPTARG ;;
+   esac
+done
+
+
+if [ -z "$DOMAIN" ];then
+   echo -e "\nERROR: -d <domain> is a required argument. \n"
+   echo -e "NOTE: if -r <document root> and -p <port number> are not entered, the defaults '/var/www/vhosts/' and port 80 will be used.\n"
+   exit 1
+fi
+
+
+if [ -f /etc/redhat-release ]; then
+        echo "Redhat based system located"
+        DISTRO="Redhat"
+        if [ -f /etc/httpd/vhost.d/$DOMAIN.conf ]; then
+                echo "Virtual Host already exists"
+                exit
+        fi
+fi
+if [ -f /etc/debian_version ]; then
+        echo "Debian based System located"
+        DISTRO="Debian"
+        if [ ! -f /etc/apache2/sites-enabled/$DOMAIN ]; then
+                if [ -f /etc/apache2/sites-available/$DOMAIN ]; then
+                        echo "Virtual Host already exists"
+                fi
+        else
+                echo "Virtual Host already exists"
+                exit
+        fi
+fi
+
+#script from justcurl.com written by Lindsey Anderson only minor tweaks made to work with argument variables
+
+
+DATA="<VirtualHost *:$PORT>
+        ServerName $DOMAIN
+        ServerAlias www.$DOMAIN
+        #### This is where you put your files for that domain: $DOCROOT/$DOMAIN
+        DocumentRoot $DOCROOT/$DOMAIN
+
+        #RewriteEngine On
+        #RewriteCond %{HTTP_HOST} ^$DOMAIN
+        #RewriteRule ^(.*)$ http://www.$DOMAIN$1 [R=301,L]
+
+        <Directory $DOCROOT/$DOMAIN>
+                Options -Indexes +FollowSymLinks -MultiViews
+                AllowOverride All
+                Order deny,allow
+                Allow from all
+        </Directory>"
+if [[ "$DISTRO" == "Debian" ]]; then
+        DATA=$DATA"
+        CustomLog /var/log/apache2/$DOMAIN-access.log combined
+        ErrorLog /var/log/apache2/$DOMAIN-error.log"
+elif [[ "$DISTRO" == "Redhat" ]]; then
+        DATA=$DATA"
+        CustomLog /var/log/httpd/$DOMAIN-access.log combined
+        ErrorLog /var/log/httpd/$DOMAIN-error.log"
+fi
+DATA=$DATA"
+        # New Relic PHP override
+        <IfModule php5_module>
+               php_value newrelic.appname "$DOMAIN"
+        </IfModule>
+        # Possible values include: debug, info, notice, warn, error, crit,
+        # alert, emerg.
+        LogLevel warn
+</VirtualHost>
+
+
+##
+# To install the SSL certificate, please place the certificates in the following files:
+# >> SSLCertificateFile    /etc/pki/tls/certs/$DOMAIN.crt
+# >> SSLCertificateKeyFile    /etc/pki/tls/private/$DOMAIN.key
+# >> SSLCACertificateFile    /etc/pki/tls/certs/$DOMAIN.ca.crt
+#
+# After these files have been created, and ONLY AFTER, then run this and restart Apache:
+#
+# To remove these comments and use the virtual host, use the following:
+# VI   -  :39,$ s/^#//g
+# RedHat Bash -  sed -i '39,$ s/^#//g' /etc/httpd/vhost.d/$DOMAIN.conf && service httpd reload
+# Debian Bash -  sed -i '39,$ s/^#//g' /etc/apache2/sites-available/$DOMAIN && service apache2 reload
+##
+
+#<VirtualHost _default_:443>
+#        ServerName $DOMAIN
+#        ServerAlias www.$DOMAIN
+#        DocumentRoot $DOCROOT/$DOMAIN
+#        <Directory $DOCROOT/$DOMAIN>
+#                Options -Indexes +FollowSymLinks -MultiViews
+#                AllowOverride All
+#        </Directory>
+#"
+if [[ "$DISTRO" == "Debian" ]]; then
+        DATA=$DATA"
+#        CustomLog /var/log/apache2/$DOMAIN-ssl-access.log combined
+#        ErrorLog /var/log/apache2/$DOMAIN-ssl-error.log"
+elif [[ "$DISTRO" == "Redhat" ]]; then
+        DATA=$DATA"
+#        CustomLog /var/log/httpd/$DOMAIN-ssl-access.log combined
+#        ErrorLog /var/log/httpd/$DOMAIN-ssl-error.log"
+fi
+DATA=$DATA"
+#
+#        # Possible values include: debug, info, notice, warn, error, crit,
+#        # alert, emerg.
+#        LogLevel warn
+#
+#        SSLEngine on"
+if [[ "$DISTRO" == "Debian" ]]; then
+        DATA=$DATA"
+#        SSLCertificateFile    /etc/ssl/certs/2014-$DOMAIN.crt
+#        SSLCertificateKeyFile /etc/ssl/private/2014-$DOMAIN.key
+#        SSLCACertificateFile /etc/ssl/certs/2014-$DOMAIN.ca.crt
+#"
+elif [[ "$DISTRO" == "Redhat" ]]; then
+        DATA=$DATA"
+#        SSLCertificateFile    /etc/pki/tls/certs/2014-$DOMAIN.crt
+#        SSLCertificateKeyFile /etc/pki/tls/private/2014-$DOMAIN.key
+#        SSLCACertificateFile /etc/pki/tls/certs/2014-$DOMAIN.ca.crt
+#"
+fi
+DATA=$DATA"
+#        <IfModule php5_module>
+#                php_value newrelic.appname "$DOMAIN"
+#        </IfModule>
+#        <FilesMatch \\\"\.(cgi|shtml|phtml|php)\$\\\">
+#                SSLOptions +StdEnvVars
+#        </FilesMatch>
+#
+#        BrowserMatch \\\"MSIE [2-6]\\\" \\
+#                nokeepalive ssl-unclean-shutdown \\
+#                downgrade-1.0 force-response-1.0
+#        BrowserMatch \\\"MSIE [17-9]\\\" ssl-unclean-shutdown
+#</VirtualHost>"
+
+
+if [[ "$DISTRO" == "Redhat" ]]; then
+        # Check for vhost directory in /etc/httpd
+        if [ ! -d /etc/httpd/vhost.d  ]; then
+                mkdir /etc/httpd/vhost.d &&
+                echo "include vhost.d/*.conf" >> /etc/httpd/conf/httpd.conf
+        fi
+        if [ -f /etc/httpd/vhost.d/$DOMAIN.conf ]; then
+                echo "This virtual host already exists on this system."
+                echo "Please remove the virtual host configuration file."
+                exit 1
+        fi
+        echo "$DATA" > /etc/httpd/vhost.d/$DOMAIN.conf &&
+        mkdir -p $DOCROOT/$DOMAIN
+        #chown apache:apache /$DOCROOT/$DOMAIN &&
+        #chmod 2775 $DOCROOT/$DOMAIN
+
+elif [[ "$DISTRO" == "Debian" ]]; then
+        if [ -f /etc/apache2/sites-available/$DOMAIN ]; then
+                echo "This virtual host already exists on this system."
+                echo "Please remove the virtual host configuration file."
+                exit 1
+        fi
+        echo "$DATA" > /etc/apache2/sites-available/$DOMAIN &&
+        mkdir -p $DOCROOT/$DOMAIN
+        #chown www-data:www-data $DOCROOT/$DOMAIN &&
+        #chmod 2775 $DOCROOT/$DOMAIN &&
+        ln -s /etc/apache2/sites-available/$DOMAIN /etc/apache2/sites-enabled/domain.com
+fi
+
+echo "********************"
+echo ">> Server Name : $DOMAIN"
+echo ">> Server Alias: www.$DOMAIN"
+echo ">> Document Root: $DOCROOT/$DOMAIN"
+echo "********************"
