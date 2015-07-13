8ae8ff571b35c33b298756b09cf1eb273aa50696
drogomarks <rudy.marks@gmail.com>
2015-07-13 11:36:25 -0500

    Update nginxS.sh

--- nginxS.sh
+++ nginxS.sh
@@ -24,18 +24,46 @@ sleep 1
 #Got it working now by creating a loop for 80, 443 and anything else under Alts.
 
 if [[ "$DISTRO" == "RedHat" ]]; then
+        
+	echo -e "The following vhosts directories have been located: \f"
+
+	if [ -d /etc/nginx/vhost.d/ ]; then
+		CONFS1=$(ls /etc/nginx/vhost.d/*.conf | sort)
+		echo -e "- vhost.d/ vhosts directory found. \n"
+		sleep 1
+	fi
+
+	if [ -d /etc/nginx/sites-available/ ]; then
+		CONFS2=$(ls /etc/nginx/sites-available/*.conf | sort)
+		echo -e "- sites-available/ vhosts directory found. \n"
+		sleep 1
+	fi
+	
+	if [ -d /etc/nginx/conf.d/ ]; then
+		CONFS=$(ls /etc/nginx/conf.d/*.conf | sort)
+		echo -e "- conf.d/ vhosts directory found. \n"
+		sleep .5
+        fi
+
+
         echo -e "===========================================================================   "
         echo -e "Nginx Tool has found the follwing vhosts configured on the following ports:   "
         echo -e "=========================================================================== \f"
-        if [ -d /etc/nginx/vhosts.d/ ]; then
-                CONFS=$(ls /etc/nginx/vhosts.d/*.conf | sort)
-        else
-                if [ -d /etc/nginx/sites-avaiable/ ];then
-                        CONFS=$(ls /etc/nginx/sites-available/*.conf | sort)
-        else 
-        CONFS=$(ls /etc/nginx/conf.d/*.conf | sort)
-        fi
-        
+
+
+
+############# $CONFS
+
+if [[ -z $CONFS ]];then
+	exit
+else
+
+        echo -e "\n-------------"
+        echo -e "From conf.d/"
+        echo -e "------------- \f"
+
+	sleep 1
+
         echo -e "HTTP: "
 
         for i in  $CONFS;
@@ -79,9 +107,124 @@ if [[ "$DISTRO" == "RedHat" ]]; then
                         fi
         done | column -t | sort -rk 2,2
 
-        nginx -t
 
 fi
+
+############# $CONFS1
+if [[ -z $CONFS1 ]];then
+	exit
+else
+
+        echo -e "\n-------------"
+        echo -e "From vhost.d/"
+        echo -e "------------- \f"
+
+	sleep 1
+	
+        echo -e "HTTP: "
+
+        for i in  $CONFS1;
+                do
+                        PORT=`cat $i | grep listen | grep -o '[80]\+'  | awk 'NR==1'`
+                        DOMAIN=`cat $i | grep -m1 server_name | awk '{print $2}' | sed 's/;//g'`
+                        if [[ "$DOMAIN" == "server_name" ]]; then
+                                DOMAIN=`cat $i | grep -m1 server_name | awk '{print $3}' | sed 's/;//g'`
+                        fi
+                        if [[ "$PORT" == "80" ]]; then
+                                echo -e "$DOMAIN :$PORT ($i) \f"
+                        fi
+                done | column -t | sort -rk 2,2
+
+        echo -e "HTTPS:"
+
+        for i in  $CONFS1;
+                do
+                        PORT=`cat $i | grep listen | grep -o '[443]\+'  | awk 'NR==1'`
+                        DOMAIN=`cat $i | grep -m1 server_name | awk '{print $2}' | sed 's/;//g'`
+                        if [[ "$DOMAIN" == "server_name" ]]; then
+                                DOMAIN=`cat $i | grep -m1 server_name | awk '{print $3}' | sed 's/;//g'`
+                        fi
+                        if [[ "$PORT" == "443" ]]; then
+                        echo -e "$DOMAIN :$PORT ($i) \f"
+                        fi
+                done | column -t | sort -rk 2,2
+
+
+        echo -e "Alt. Ports: "
+
+        for i  in  $CONFS1;
+                do
+                        PORT=`cat $i | grep listen | grep -o '[0-9]\+' | awk 'NR==1'`
+                        DOMAIN=`cat $i | grep -m1 server_name | awk '{print $2}' | sed 's/;//g'`
+                        if [[ "$DOMAIN" == "server_name" ]]; then
+                                DOMAIN=`cat $i | grep -m1 server_name | awk '{print $3}' | sed 's/;//g'`
+                        fi
+                        if [[ "$PORT" != "80" && "$PORT" != "443" ]]; then
+                        echo -e "$DOMAIN :$PORT ($i) \f"
+                        fi
+        done | column -t | sort -rk 2,2
+
+
+fi
+
+############# $CONFS2
+
+if [ -z $CONFS2 ]; then
+	exit
+else
+
+        echo -e "\n---------------------"
+        echo -e "From sites-available/"
+        echo -e "--------------------- \f"
+
+	sleep 1
+
+        echo -e "HTTP: "
+
+        for i in  $CONFS2;
+                do
+                        PORT=`cat $i | grep listen | grep -o '[80]\+'  | awk 'NR==1'`
+                        DOMAIN=`cat $i | grep -m1 server_name | awk '{print $2}' | sed 's/;//g'`
+                        if [[ "$DOMAIN" == "server_name" ]]; then
+                                DOMAIN=`cat $i | grep -m1 server_name | awk '{print $3}' | sed 's/;//g'`
+                        fi
+                        if [[ "$PORT" == "80" ]]; then
+                                echo -e "$DOMAIN :$PORT ($i) \f"
+                        fi
+                done | column -t | sort -rk 2,2
+
+        echo -e "HTTPS:"
+
+        for i in  $CONFS2;
+                do
+                        PORT=`cat $i | grep listen | grep -o '[443]\+'  | awk 'NR==1'`
+                        DOMAIN=`cat $i | grep -m1 server_name | awk '{print $2}' | sed 's/;//g'`
+                        if [[ "$DOMAIN" == "server_name" ]]; then
+                                DOMAIN=`cat $i | grep -m1 server_name | awk '{print $3}' | sed 's/;//g'`
+                        fi
+                        if [[ "$PORT" == "443" ]]; then
+                        echo -e "$DOMAIN :$PORT ($i) \f"
+                        fi
+                done | column -t | sort -rk 2,2
+
+
+        echo -e "Alt. Ports: "
+
+        for i  in  $CONFS2;
+                do
+                        PORT=`cat $i | grep listen | grep -o '[0-9]\+' | awk 'NR==1'`
+                        DOMAIN=`cat $i | grep -m1 server_name | awk '{print $2}' | sed 's/;//g'`
+                        if [[ "$DOMAIN" == "server_name" ]]; then
+                                DOMAIN=`cat $i | grep -m1 server_name | awk '{print $3}' | sed 's/;//g'`
+                        fi
+                        if [[ "$PORT" != "80" && "$PORT" != "443" ]]; then
+                        echo -e "$DOMAIN :$PORT ($i) \f"
+                        fi
+        done | column -t | sort -rk 2,2
+
+	fi        
+fi
+nginx -t
 
 
 #Essentially the same thing but with Debian/Ubuntu
