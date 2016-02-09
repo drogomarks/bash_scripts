2035f88809cbdc528b7953fbbbad3aa564d2b61b
Marks <marksaws@u8cdcd44c1d76562689f9.ant.amazon.com>
2016-02-09 14:03:33 -0800

    Updated for RHEL 7/Varnish 4

--- wpstack_deploy.sh
+++ wpstack_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Script to quickly deploy a Stack designed with WordPress in mind that consists of Nginx -> Varnish -> Apache
+# Script to quickly deploy a Stack designed with WordPress in mind that consists of:
+# Nginx -> Varnish -> Apache
 # Use at your own risk
 #Disclaimer:
 
@@ -8,11 +9,12 @@ echo -e "****WARNING****\n"
 echo -e "This script is intended to be run on a new server (with nothing on it currently) and deploys the following stack for WordPress:\n"
 echo -e "Nginx (for static content) -> Varnish (for whatever Nginx does not cache/serve) -> Apache (for PHP).\n"
 
-echo -e "---The following Packages will be installed---\n"
+echo -e "---The following Packages will be installed and system packages update will be run---\n"
 echo -e " - Nginx"  
 echo -e " - Varnish"  
 echo -e " - Apache"  
 echo -e " - PHP (and dependencies)"  
+echo -e " - Epel Repo added for RHEL 7"
 echo -e " - MySQL (if you opt for it)\n"
 
 echo -e "Do you wish to proceed? (y/n)"
@@ -24,7 +26,10 @@ if [ $PROCEED_ANSWER == 'n' ]; then
 fi
 
 #Can I haz OS?
-if [ -f /etc/redhat-release ]; then
+if [[ `cat /etc/redhat-releasae` == *"7"* ]]; then
+	echo "Redhat 7 based system located"
+	DISTRO="RedHat7"
+else
 	echo "Redhat based system located"
 	DISTRO="RedHat"
 fi
@@ -44,12 +49,127 @@ if [[ `cat /etc/issue | grep -i Amazon | awk {'print $1'}` == "Amazon" ]]; then
 	DISTRO="Amazon"
 fi
 
-####################
-##   RHEL Based    #
-####################
+
+
+##############################################
+##   RHEL/CentOS 6 or Amazon Linux  Based    #
+##############################################
 
 #Update Packages
-if [[ "$DISTRO" == "RedHat" ]] || [[ "Amazon" ]]; then
+if [ "$DISTRO" == "RedHat7" ]; then
+	echo -e "Checking for Epel Repo.."
+	if [ `yum repolist | grep epel` != *"epel"* ]; then
+		wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm > /tmp/
+		yum install epel-release-7.noarch.rpm
+	else
+		echo -e "Epel Repo installed already, moving on.\n"
+		sleep .5
+	fi 
+
+	echo -e "Updating packages..."
+	sleep .5 
+	yum update
+
+	#Install packages
+	echo -e "Installing pacakges now.."
+	sleep .5
+	yum install php php-mysql nginx httpd varnish mysql-client
+
+	echo "Do you want to install MySQL on this server? (y/n)?"
+	read MYSQL_ANSWER
+
+	#Local MySQL?
+	if [ $MYSQL_ANSWER == 'y' ]; then
+        	echo "Installing MySQL Server..."
+        	sleep .5
+        	yum install mysql-server
+
+
+	fi
+
+
+	#Domain?
+	echo -e "Would you like to configure a domain already? (y/n)"
+	read DOMAIN_ANSWER
+
+	if [ $DOMAIN_ANSWER == "n" ]; then 
+   		echo -e "Ok. Moving on."
+		else 
+		echo -e "Domain name?:"
+		read DOMAIN
+	fi
+
+
+	################### CONFIGURE APACHE ##########################
+	echo -e "Configuring Apache...\n" 
+
+	#Create VirtualHosts Directory & include it
+	mkdir -p /var/www/vhosts/
+	mkdir -p /etc/httpd/vhost.d/
+	echo "IncludeOptional vhost.d/*.conf" >> /etc/httpd/conf/httpd.conf
+
+	#Make Apache Listen on 8080
+	sed -i 's/80/8080/' /etc/httpd/conf/httpd.conf
+
+	if [ -f /etc/httpd/ports.conf ]; then 
+		sed -i 's/80/8080/' /etc/httpd/ports.conf
+	fi
+
+	#Set up domain
+	if [ $DOMAIN_ANSWER = 'y' ]; then
+		echo -e "Setting up your domain $DOMAIN in Apache...\n"
+		wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/vhosts.sh &> /dev/null && bash vhosts.sh -d $DOMAIN -p 8080
+	fi 
+
+	service httpd restart
+
+	echo -e "Done!"
+	sleep 1
+
+	################### CONFIGURE VARNISH #########################
+	echo -e "Configuring Varnish...\n"
+
+	sed -i 's/6081/8081/g' /etc/varnish/varnish.params
+
+	cp /etc/varnish/default.vcl /etc/varnish/default.vcl.orig
+
+	wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/files/wordpress_default_v4.vcl &> /dev/null && mv wordpress_default_v4.vcl /etc/varnish/default.vcl
+
+	service varnish restart
+	echo -e "DONE!\n"
+	sleep 1
+
+
+	##################### CONFIGURE NGINX ########################
+	echo -e "Configuring Nginx...\n"
+
+	cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
+
+	wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/files/default_nginx.conf &> /dev/null && mv default_nginx.conf /etc/nginx/nginx.conf
+
+	touch /etc/nginx/conf.d/global.deny
+
+	wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/files/wpStack_nginx_vhost.conf &> /dev/null && mv wpStack_nginx_vhost.conf /etc/nginx/conf.d/default_template.conf
+
+	#Set up domain 
+	if [ $DOMAIN_ANSWER = 'y' ]; then
+		cp /etc/nginx/conf.d/default_template.conf /etc/nginx/conf.d/$DOMAIN.conf
+		sed -i "s/example.com/$DOMAIN/g" /etc/nginx/conf.d/$DOMAIN.conf
+	fi
+
+	service nginx restart 
+	echo -e "DONE!\n"
+	sleep 1
+
+
+fi
+
+##############################################
+##   RHEL/CentOS 6 or Amazon Linux  Based    #
+##############################################
+
+#Update Packages
+if [ "$DISTRO" == "RedHat" ] || [ "$DISTRO" == "Amazon" ]; then
 	echo -e "Updating packages first..."
 	sleep .5 
 	yum update
@@ -113,7 +233,7 @@ if [[ "$DISTRO" == "RedHat" ]] || [[ "Amazon" ]]; then
 	################### CONFIGURE VARNISH #########################
 	echo -e "Configuring Varnish...\n"
 
-	sed -i 's/80/8081/g' /etc/sysconfig/varnish
+	sed -i 's/6081/8081/g' /etc/sysconfig/varnish
 
 	cp /etc/varnish/default.vcl /etc/varnish/default.vcl.orig
 
