468110e6664be175af23e3d82f76201c3c1dd38d
Marks <marksaws@u8cdcd44c1d76562689f9.ant.amazon.com>
2016-02-10 14:07:41 -0800

    commented out RHEL 7 portion, does not work properly.

--- wpstack_deploy.sh
+++ wpstack_deploy.sh
@@ -29,7 +29,9 @@ fi
 #RHEL 7 or 6
 if [ -f /etc/redhat-release ]; then
 	if [[ `cat /etc/redhat-release` == *"7"* ]];then
-		echo "RedHat 7 System detected"
+		echo -e "RedHat 7 System detected."
+		echo -e "RedHat 7 is currently not supported...but I'm working on it!"
+		echo -e "kthxbai!\n"
 		DISTRO="RedHat7"
 	else
 		echo "RedHat 6 or earlier detected"
@@ -51,117 +53,117 @@ fi
 
 
 
-####################
-##   RHEL/CentOS 7 #
-####################
-
-#Update Packages
-if [ "$DISTRO" == "RedHat7" ]; then
-	echo -e "Checking for Epel Repo.."
-	if [[ -z `yum repolist | grep epel` ]]; then
-		wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-		yum install epel-release-latest-7.noarch.rpm
-	else
-		echo -e "Epel Repo installed already, moving on.\n"
-		sleep .5
-	fi 
-
-	echo -e "Updating packages..."
-	sleep .5 
-	yum update
-
-	#Install packages
-	echo -e "Installing pacakges now.."
-	sleep .5
-	yum install php php-mysql nginx httpd varnish mysql-client
-
-	echo "Do you want to install MySQL on this server? (y/n)?"
-	read MYSQL_ANSWER
-
-	#Local MySQL?
-	if [ $MYSQL_ANSWER == 'y' ]; then
-        	echo "Installing MySQL Server..."
-        	sleep .5
-        	yum install mysql-server
-
-
-	fi
-
-
-	#Domain?
-	echo -e "Would you like to configure a domain already? (y/n)"
-	read DOMAIN_ANSWER
-
-	if [ $DOMAIN_ANSWER == "n" ]; then 
-   		echo -e "Ok. Moving on."
-		else 
-		echo -e "Domain name?:"
-		read DOMAIN
-	fi
-
-
-	################### CONFIGURE APACHE ##########################
-	echo -e "Configuring Apache...\n" 
-
-	#Create VirtualHosts Directory & include it
-	mkdir -p /var/www/vhosts/
-	mkdir -p /etc/httpd/vhost.d/
-	echo "IncludeOptional vhost.d/*.conf" >> /etc/httpd/conf/httpd.conf
-
-	#Make Apache Listen on 8080
-	sed -i 's/80/8080/' /etc/httpd/conf/httpd.conf
-
-	if [ -f /etc/httpd/ports.conf ]; then 
-		sed -i 's/80/8080/' /etc/httpd/ports.conf
-	fi
-
-	#Set up domain
-	if [ $DOMAIN_ANSWER = 'y' ]; then
-		echo -e "Setting up your domain $DOMAIN in Apache...\n"
-		wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/vhosts.sh &> /dev/null && bash vhosts.sh -d $DOMAIN -p 8080
-	fi 
-
-	service httpd restart
-
-	echo -e "Done!"
-	sleep 1
-
-	################### CONFIGURE VARNISH #########################
-	echo -e "Configuring Varnish...\n"
-
-
-	cp /etc/varnish/default.vcl /etc/varnish/default.vcl.orig
-
-	wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/files/wordpress_default_v4.vcl &> /dev/null && mv wordpress_default_v4.vcl /etc/varnish/default.vcl
-
-	service varnish restart
-	echo -e "DONE!\n"
-	sleep 1
-
-
-	##################### CONFIGURE NGINX ########################
-	echo -e "Configuring Nginx...\n"
-
-	cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
-
-	wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/files/default_nginx.conf &> /dev/null && mv default_nginx.conf /etc/nginx/nginx.conf
-	
-	touch /etc/nginx/conf.d/global.deny
-
-	wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/files/wpStack_nginx_vhost_rhel7.conf &> /dev/null && mv wpStack_nginx_vhost_rhel7.conf /etc/nginx/conf.d/default_template.conf
-
-	#Set up domain 
-	if [ $DOMAIN_ANSWER = 'y' ]; then
-		cp /etc/nginx/conf.d/default_template.conf /etc/nginx/conf.d/$DOMAIN.conf
-		sed -i "s/example.com/$DOMAIN/g" /etc/nginx/conf.d/$DOMAIN.conf
-	fi
-
-	service nginx restart 
-	echo -e "DONE!\n"
-	sleep 1
-
-
-fi
+#####################
+###   RHEL/CentOS 7 # NOT WORKING YET. Only uncomment if your going to fix it for me: https://github.com/drogomarks/bash_scripts/blob/master/wpstack_deploy.sh
+#####################
+#
+##Update Packages
+#if [ "$DISTRO" == "RedHat7" ]; then
+#	echo -e "Checking for Epel Repo.."
+#	if [[ -z `yum repolist | grep epel` ]]; then
+#		wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#		yum install epel-release-latest-7.noarch.rpm
+#	else
+#		echo -e "Epel Repo installed already, moving on.\n"
+#		sleep .5
+#	fi 
+#
+#	echo -e "Updating packages..."
+#	sleep .5 
+#	yum update
+#
+#	#Install packages
+#	echo -e "Installing pacakges now.."
+#	sleep .5
+#	yum install php php-mysql nginx httpd varnish mysql-client
+#
+#	echo "Do you want to install MySQL on this server? (y/n)?"
+#	read MYSQL_ANSWER
+#
+#	#Local MySQL?
+#	if [ $MYSQL_ANSWER == 'y' ]; then
+#        	echo "Installing MySQL Server..."
+#        	sleep .5
+#        	yum install mysql-server
+#
+#
+#	fi
+#
+#
+#	#Domain?
+#	echo -e "Would you like to configure a domain already? (y/n)"
+#	read DOMAIN_ANSWER
+#
+#	if [ $DOMAIN_ANSWER == "n" ]; then 
+#   		echo -e "Ok. Moving on."
+#		else 
+#		echo -e "Domain name?:"
+#		read DOMAIN
+#	fi
+#
+#
+#	################### CONFIGURE APACHE ##########################
+#	echo -e "Configuring Apache...\n" 
+#
+#	#Create VirtualHosts Directory & include it
+#	mkdir -p /var/www/vhosts/
+#	mkdir -p /etc/httpd/vhost.d/
+#	echo "IncludeOptional vhost.d/*.conf" >> /etc/httpd/conf/httpd.conf
+#
+#	#Make Apache Listen on 8080
+#	sed -i 's/80/8080/' /etc/httpd/conf/httpd.conf
+#
+#	if [ -f /etc/httpd/ports.conf ]; then 
+#		sed -i 's/80/8080/' /etc/httpd/ports.conf
+#	fi
+#
+#	#Set up domain
+#	if [ $DOMAIN_ANSWER = 'y' ]; then
+#		echo -e "Setting up your domain $DOMAIN in Apache...\n"
+#		wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/vhosts.sh &> /dev/null && bash vhosts.sh -d $DOMAIN -p 8080
+#	fi 
+#
+#	service httpd restart
+#
+#	echo -e "Done!"
+#	sleep 1
+#
+#	################### CONFIGURE VARNISH #########################
+#	echo -e "Configuring Varnish...\n"
+#
+#
+#	cp /etc/varnish/default.vcl /etc/varnish/default.vcl.orig
+#
+#	wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/files/wordpress_default_v4.vcl &> /dev/null && mv wordpress_default_v4.vcl /etc/varnish/default.vcl
+#
+#	service varnish restart
+#	echo -e "DONE!\n"
+#	sleep 1
+#
+#
+#	##################### CONFIGURE NGINX ########################
+#	echo -e "Configuring Nginx...\n"
+#
+#	cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
+#
+#	wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/files/default_nginx.conf &> /dev/null && mv default_nginx.conf /etc/nginx/nginx.conf
+#	
+#	touch /etc/nginx/conf.d/global.deny
+#
+#	wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/files/wpStack_nginx_vhost_rhel7.conf &> /dev/null && mv wpStack_nginx_vhost_rhel7.conf /etc/nginx/conf.d/default_template.conf
+#
+#	#Set up domain 
+#	if [ $DOMAIN_ANSWER = 'y' ]; then
+#		cp /etc/nginx/conf.d/default_template.conf /etc/nginx/conf.d/$DOMAIN.conf
+#		sed -i "s/example.com/$DOMAIN/g" /etc/nginx/conf.d/$DOMAIN.conf
+#	fi
+#
+#	service nginx restart 
+#	echo -e "DONE!\n"
+#	sleep 1
+#
+#
+#fi
 
 ##############################################
 ##   RHEL/CentOS 6 or Amazon Linux  Based    #
