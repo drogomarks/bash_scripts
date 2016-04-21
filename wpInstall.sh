aaefa916b3c0a0098e061c2d230b64be482b44bb
drogomarks <marksaws@u8cdcd44c1d76562689f9.ant.amazon.com>
2016-04-21 10:29:52 -0700

    offers LAMP install

--- wpInstall.sh
+++ wpInstall.sh
@@ -1,4 +1,169 @@
 #!/bin/bash
+echo -e "Do you need to install LAMP on this server?" 
+read LAMP_ANSWER
+
+if [ $LAMP_ANSWER == 'y' ];then
+
+#!/bin/bash
+
+#Can I haz OS?
+#RHEL 7 or 6
+if [ -f /etc/redhat-release ]; then
+	if [[ `cat /etc/redhat-release` == *"7"* ]];then
+		echo -e "RedHat 7 System detected."
+		echo -e "RedHat 7 is currently not supported...but I'm working on it!"
+		echo -e "kthxbai!\n"
+		DISTRO="RedHat7"
+	else
+		echo "RedHat 6 or earlier detected"
+		DISTRO="RedHat"
+	fi
+fi
+
+#Debian
+if [ -f /etc/debian_version ]; then
+		echo "Debian based System located"
+		DISTRO="Debian"
+fi
+
+#Amazon Linux
+if [[ `cat /etc/issue | grep -i Amazon | awk {'print $1'}` == "Amazon" ]]; then
+	echo "Amazon Linux (RHEL Based) system located"
+	DISTRO="Amazon"
+fi
+
+
+
+
+#Update Packages
+if [ "$DISTRO" == "RedHat" ] || [ "$DISTRO" == "Amazon" ]; then
+	echo -e "Updating packages first..."
+	sleep .5 
+	yum update
+
+	#Install packages
+	echo -e "Installing pacakges now.."
+	sleep .5
+	yum install php php-mysql httpd mysql-client
+
+	echo "Do you want to install MySQL on this server? (y/n)?"
+	read MYSQL_ANSWER
+
+	#Local MySQL?
+	if [ $MYSQL_ANSWER == 'y' ]; then
+        	echo "Installing MySQL Server..."
+        	sleep .5
+        	yum install mysql-server
+
+
+	fi
+
+
+	#Domain?
+	echo -e "Would you like to configure a domain already? (y/n)"
+	read DOMAIN_ANSWER
+
+	if [ $DOMAIN_ANSWER == "n" ]; then 
+   		echo -e "Ok. Moving on."
+		else 
+		echo -e "Domain name?:"
+		read DOMAIN
+	fi
+
+
+	################### CONFIGURE APACHE ##########################
+	echo -e "Configuring Apache...\n" 
+
+	#Create VirtualHosts Directory & include it
+	mkdir -p /var/www/vhosts/
+	mkdir -p /etc/httpd/vhost.d/
+	echo "Include vhost.d/*.conf" >> /etc/httpd/conf/httpd.conf
+
+
+	#Set up domain
+	if [ $DOMAIN_ANSWER = 'y' ]; then
+		echo -e "Setting up your domain $DOMAIN in Apache...\n"
+		wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/vhosts.sh &> /dev/null && bash vhosts.sh -d $DOMAIN
+	fi 
+
+	service httpd restart
+
+	echo -e "Done!"
+	sleep 1
+
+
+
+####################
+#    Debian based   #
+####################
+
+#Update Packagesa
+if [[ "$DISTRO" == "Debian" ]]; then
+	echo -e "Updating packages first..."
+	sleep .5 
+	apt-get update
+
+	#Install packages
+	echo -e "Installing pacakges now.."
+	sleep .5
+	apt-get install php5 libapache2-mod-php5 php5-mcrypt php5-mysql mysql-client
+
+	echo "Do you want to install MySQL on this server? (y/n)?"
+	read MYSQL_ANSWER
+
+		#Local MySQL?
+	if [ $MYSQL_ANSWER == 'y' ]; then
+        	echo "Installing MySQL Server..."
+        	sleep .5
+        	apt-get install mysql-server
+
+
+	fi
+
+
+	#Domain?
+	echo -e "Would you like to configure a domain already? (y/n)"
+	read DOMAIN_ANSWER
+
+	if [ $DOMAIN_ANSWER == "n" ]; then 
+   		echo -e "Ok. Moving on."
+	else 
+		echo -e "Domain name?:"
+		read DOMAIN
+	fi
+
+
+	################### CONFIGURE APACHE ##########################
+	echo -e "Configuring Apache...\n" 
+
+	#Create VirtualHosts Directory & include it
+	mkdir -p /var/www/vhosts/
+
+	#Disabled any default sites enabled on port 80
+	SITES_ENABLED=`a2query -s | awk {'print $1'}`
+	for site in $SITES_ENABLED;
+	do 
+		a2dissite $site
+	done	
+
+
+	#Set up domain
+	if [ $DOMAIN_ANSWER = 'y' ]; then
+		echo -e "Setting up your domain $DOMAIN in Apache...\n"
+		wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/vhosts.sh &> /dev/null && bash vhosts.sh -d $DOMAIN
+	fi 
+
+	service apache2 restart
+
+	echo -e "Done!"
+	sleep 1
+
+    fi
+
+fi
+
+else 
+
 
 echo -e "What directory are you installing Wordpress to? (FULL path)\n"
 read WP_DIR
