9b15a195de98295533b55c615477864f0ebde1a3
drogomarks <marksaws@u8cdcd44c1d76562689f9.ant.amazon.com>
2016-05-27 14:10:22 -0700

    i'm dumb

--- wpstack_deploy.sh
+++ wpstack_deploy.sh
@@ -333,7 +333,6 @@ if [[ "$DISTRO" == "Debian" ]]; then
 	if [ $DOMAIN_ANSWER = 'y' ]; then
 		echo -e "Setting up your domain $DOMAIN in Apache...\n"
 		wget https://raw.githubusercontent.com/drogomarks/bash_scripts/master/vhosts.sh &> /dev/null && bash vhosts.sh -d $DOMAIN -p 8080
-		echo "\n Your new Virtual Host infomration:\n"
 		sleep 1
 	fi 
 
