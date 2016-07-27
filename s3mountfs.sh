5f20e8447f8ea7ae474d1771c66eba6922437ac6
drogomarks <marksaws@u8cdcd44c1d76562689f9.ant.amazon.com>
2016-07-27 15:45:01 -0700

    added some comments

--- s3mountfs.sh
+++ s3mountfs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# Determine the OS
+# Simple script to install S3FS/Fuse to allow the mounting of a bucket to Linux 
 
+# Ensure user is root
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
+# Determine the OS
 if [ -f /etc/redhat-release ]; then
         echo "Red Hat based sytem detected."
         DISTRO="RedHat"
@@ -27,24 +29,23 @@ fi
 
 # Install dependencies 
 
-
+# RHEL/CentOS/Amazon
 if [ $DISTRO == "RedHat" ]; then
 	echo -e "RHEL based OS detected."
 	yum install gcc libstdc++-devel gcc-c++ curl-devel libxml2-devel openssl-devel mailcap fuse-libs
 
 fi
 
+# Debian/Ubuntu
 if [ $DISTRO = "Debian" ]; then
 	echo -e "Debian based OS detected."
 	apt-get install build-essential libcurl4-openssl-dev libxml2-dev mime-support fuse-libs
 
 fi
+
 # Download and install Latest Fuse
-
 wget https://github.com/libfuse/libfuse/releases/download/fuse-2.9.7/fuse-2.9.7.tar.gz
-
 mv fuse-2.9.7.tar.gz /usr/src/ 
-
 tar xvf /usr/src/fuse-2.9.7.tar.gz  -C /usr/src/
 
 cd /usr/src/fuse-2.9.7/ && ./configure --prefix=/usr/local && make && make install
@@ -56,34 +57,32 @@ modprobe fuse
 
 
 #Download and install S3FS
-
 wget https://s3fs.googlecode.com/files/s3fs-1.74.tar.gz 
 mv s3fs-1.74.tar.gz /usr/src/
-
 tar xvf /usr/src/s3fs-1.74.tar.gz -C /usr/src/
 
 cd /usr/src/s3fs-1.74/ && ./configure --prefix=/usr/local && make && make install
 
 
-#Get Access Key ID and Key
-
+#Get Key ID
 while [ -z $KEY_ID ]; do
 
 	echo -e "Access Key ID:"
 	read KEY_ID
 done 
 
+#Get Access ID
 while [ -z $ACCESS_KEY ]; do
 	echo -e "Secret Access Key:"
 	read ACCESS_KEY
 done
 
-
+#Write out passwd file for S3FS
 echo "$KEY_ID:$ACCESS_KEY" > ~/.passwd-s3fs
-
 chmod 600  ~/.passwd-s3fs
 
 
+#Extra info
 echo -e "Installation Complete!\n"
 
 echo -e "NOTE: Ensure your Key ID and Access Key are correct in ~/.passwd-s3fs file!\n"
